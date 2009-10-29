1a868dd243da4326eb8a7b5b2370d8af7fe56a34
Hiroshi Hashiguchi <xcatsan@mac.com>
2009-10-29 17:18:41 +0900

    new

---
+++ Helloworld2/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Helloworld2
+//
+//  Created by ?? ? on 09/10/29.
+//  Copyright ?????????????? 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
