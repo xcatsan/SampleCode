488c9c8b8faf62e7da2067118f606611faeef5cb
Hiroshi Hashiguchi <xcatsan@mac.com>
2009-12-30 17:56:29 +0900

    Added NSBezierPathSpecialClip

---
+++ NSBezierPathSpecialClip/AppController.h
@@ -0,0 +1,16 @@
+//
+//  AppController.h
+//  NSBezierPathSpecialClip
+//
+//  Created by 橋口 湖 on 09/12/30.
+//  Copyright 2009 xcatsan.com. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface AppController : NSObject {
+
+}
+
+@end
---
+++ NSBezierPathSpecialClip/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  NSBezierPathSpecialClip
+//
+//  Created by ?? ? on 09/12/30.
+//  Copyright xcatsan.com 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
